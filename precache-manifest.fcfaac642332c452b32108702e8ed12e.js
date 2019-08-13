057bbf04b1afd5547d2e29bb6fbe3614d2763702
Ankush Malik <ankushmalik631@gmail.com>
2019-08-13 16:03:01 +0530

    Updates

---
+++ precache-manifest.fcfaac642332c452b32108702e8ed12e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a78d180c48f987f4ee1d5c573bfd58c",
+    "url": "/React_catalog/index.html"
+  },
+  {
+    "revision": "cb57ab576d9e4fc3ab48",
+    "url": "/React_catalog/static/css/main.a6a7bfd9.chunk.css"
+  },
+  {
+    "revision": "0d478b653763fee6b303",
+    "url": "/React_catalog/static/js/2.d703c3bc.chunk.js"
+  },
+  {
+    "revision": "cb57ab576d9e4fc3ab48",
+    "url": "/React_catalog/static/js/main.ae446e1e.chunk.js"
+  },
+  {
+    "revision": "625e7ad70099f1334de4",
+    "url": "/React_catalog/static/js/runtime~main.c14072e8.js"
+  }
+]);
